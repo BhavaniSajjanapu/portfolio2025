070c561e188a7e94666614189e246b03f3bee160
Bhavani Sajjanapu <bhavanisajjanapu07@gmail.com>
2025-03-20 18:53:55 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,229 @@
+import dermDetectImg from '../images/derm-detect.png';
+import fakeCurrencyImg from '../images/fake-currency .jpg';
+import htmlGeneratorImg from '../images/html-genarator.png';
+
+
+
+export const Bio = {
+  name: "Bhavani Sajjanapu",
+  roles: [
+    "Programmer",
+  ],
+  description:
+    "National Finalist - Tata Imagination Challenge 2024. Final year passionate Artificial Intelligence and Machine Learning undergraduate seeking an opportunity to kickstart my career in a tech company, where I can apply my skills and contribute to innovative solutions.",
+  github: "https://github.com/BhavaniSajjanapu",
+  resume:
+    "https://drive.google.com/file/d/1S6zISOvJsuTJVlZ_v1fPcV8c-knisYTM/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/bhavani-sajjanapu-2baa67253/",
+ 
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      
+    ],
+  },
+  {
+    title: "Programmimg",
+    skills: [
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      
+     
+    ],
+  },
+  {
+    title: "Databases",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "PL/SQL",
+        image:
+          "https://th.bing.com/th/id/R.9273ac64dd19e7b87aab1741faf42f63?rik=v%2bvteR0T7i2h8w&riu=http%3a%2f%2ftutlane.com%2fimages%2fcategorylogo%2fplsql.png&ehk=hpJvgVkn4pvHq7ce5eB5UMqKh4yIEPLAwzMxlAMrOLs%3d&risl=&pid=ImgRaw&r=0",
+      }
+     
+     
+    ],
+  },
+  {
+    title: "others",
+    skills: [
+      {
+        name: "Salesforce",
+        image:
+          "https://toppng.com/uploads/preview/salesforce-transparent-logo-11552506310l3i696ymtl.png",
+      },
+      {
+        name: "Cloud Computing",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/006/030/600/original/abstract-cloud-logo-blue-shape-cloud-computing-isolated-on-white-background-usable-for-business-and-technology-logos-flat-logo-design-template-element-vector.jpg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+     
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      
+    ],
+  },
+  {
+    title: "Soft Skills",
+    skills: [
+     
+      {
+        name: "Adaptable",
+        image: "https://thumbs.dreamstime.com/b/adaptability-icon-line-style-element-life-skills-collection-thin-templates-infographics-more-simple-filled-204151818.jpg",
+      },
+      {
+        name: "Good Communication skills",
+        image:"https://static.vecteezy.com/system/resources/previews/022/781/669/original/communication-skills-line-icon-vector.jpg",
+      },
+      {
+        name: "Teamwork",
+        image:"https://st2.depositphotos.com/2160693/42589/v/1600/depositphotos_425894660-stock-illustration-teamwork-concept-logo-team-work.jpg",
+      },
+     
+      {
+        name: "Leadership",
+        image:"https://img.freepik.com/premium-vector/team-concept-leadership-concept-vector-illustration_662848-580.jpg",
+      },
+     
+      
+    ],
+  },
+];
+
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cdn-eu.aglty.io/sunstoneuniversity/Attachments/NewItems/Malla%20Reddy%20University%20Logo_20220511141555_0.png",
+    school: "Mallareddy University",
+    date: "2021 - 2025",
+    grade: "9.18 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Mallareddy University, Hyderabad. I have completed 7 semesters and have a CGPA of 9.18",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering - Artificial Intelligence and Machine Learning",
+  },
+  {
+    id: 1,
+    img: "https://yt3.ggpht.com/a/AATXAJyE0Pnrt5t0DO3t3aSsxF_nibSfCWZySsoeew=s900-c-k-c0xffffffff-no-rj-mo",
+    school: "Sri Chaitanya Junior College",
+    date: "2021",
+    grade: "95.2%",
+    desc: "I completed my class intermediate education at Sri Chaitanya Junior College, Hyderabad",
+    degree: "Intermediate-MPC",
+  },
+  {
+    id: 2,
+    img: "https://i.ytimg.com/vi/HcR8HyDf9hM/maxresdefault.jpg",
+    school: "St.Joseph's Convent High School",
+    date: "2019",
+    grade: "9.8 CGPA",
+    desc: "I completed my class 10 education at St.Joseph's Convent High School, Adilabad.",
+    degree: "SSC",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "DERM DETECT",
+    date: "2023 - 2024",
+    description:
+      "Developed an AI-based framework using Python and Azure Cloud to analyze skin disease images with 88 percent accuracy. Leveraging the power of computer vision, this project aims to create a robust framework that can analyze skin images, extract relevant features, and employ machine learning models to classify different dermatological conditions.",
+    image:dermDetectImg,
+
+    tags: [
+      "HTML", "CSS", "Python", "REST API","javascript"
+    ],
+    category: "all",
+    github: "https://github.com/BhavaniSajjanapu/DERM-DETECT",
+    
+    
+  },
+  {
+    id: 0,
+    title: "FAKE CURRENCY DETECTOR",
+    date: "2022 - 2023",
+    description:
+      "Designed and trained a machine learning model in Python to detect fake currency by analyzing image features. Enhanced model accuracy using over 100 diverse images and optimized the code to ensure efficiency.",
+    image:fakeCurrencyImg,
+    tags: ["Machine learning", "Deep learning", "Python"],
+    category: "all ",
+    github: "https://github.com/BhavaniSajjanapu/FAKE-CURRENCY-DETECTOR",
+ 
+    
+  },
+  {
+    id: 1,
+    title: "HTML GENERATOR",
+    date: "2022 - 2023",
+    description:
+      "Automated HTML page generation by creating a Python-based framework integrated with Django. Developed RESTful APIs for communication, resulting in a 10x boost in efficiency compared to manual processes.",
+    image:htmlGeneratorImg,
+    tags: [
+      "Django", "Python"," HTML"," CSS", "HTTP", "web services"
+    ],
+    category: "all",
+    github: "https://github.com/BhavaniSajjanapu/TEXT-TO-HTML",
+    
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
